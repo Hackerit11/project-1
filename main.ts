e33102a493c8b2f07013c02a3f4da8b438f59ab4
Mega 95 <123823875+Hackerit11@users.noreply.github.com>
2023-02-03 13:43:17 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showString("KHUSSHAL NAIR A/L MAHENDRAN")
+    basic.pause(1000)
+    basic.showString("12")
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+})
